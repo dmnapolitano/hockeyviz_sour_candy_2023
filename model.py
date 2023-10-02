fc558a58d49326c0f3f4a290a79ed17e32b1b058
Diane Napolitano <dmnapolitano@gmail.com>
2023-10-01 20:32:28 -0400

    Removing centring, fixing MCMC randomness, adding points_rank as a feature

--- model.py
+++ model.py
@@ -5,51 +5,41 @@ from prophet import Prophet
 
 
 def fit_predict(team_df):
-    (train_df, test_df, centring_mean, centring_std) = preprocess(team_df, True)
-    eval_df = _go(train_df, test_df, centring_mean, centring_std)
+    (train_df, test_df) = preprocess(team_df, True)
+    eval_df = _go(train_df, test_df)
     print()
     print(eval_df)
-
-    (train_df, future_df, centring_mean, centring_std) = preprocess(team_df, False)
-    forecast_df = _go(train_df, future_df, centring_mean, centring_std)
     print()
-    print(forecast_df)
+
+    # (train_df, future_df) = preprocess(team_df, False)
+    # forecast_df = _go(train_df, future_df)
+    # print()
+    # print(forecast_df)
+    # print()
 
 
-def _go(train_df, test_df, centring_mean, centring_std):
+def _go(train_df, test_df):
     m = Prophet(yearly_seasonality=True, weekly_seasonality=False, daily_seasonality=False,
                 mcmc_samples=1000, interval_width=0.95, seasonality_prior_scale=30)
     m.add_regressor("lag1")
     m.add_regressor("lag2")
     m.add_regressor("lag3")
-    m.fit(train_df)
+    m.add_regressor("points_rank")
+    m.fit(train_df, seed=1024)
     
     pred_df = m.predict(test_df)[["ds", "yhat_lower", "yhat", "yhat_upper"]]
     pred_df["ds"] = pandas.to_datetime(pred_df["ds"])
     pred_df = pred_df.merge(test_df, on=["ds"], how="left")
-    for col in pred_df.columns:
-        if col == "ds":
-            continue
-        pred_df[col] = (pred_df[col] * centring_std) + centring_mean
 
     return pred_df
 
 
 def preprocess(team_df, model_training):
-    team_df = team_df[["season_end", "total_points"]].copy()
+    team_df = team_df[["season_end", "total_points", "points_rank"]].copy()
     team_df["ds"] = team_df["season_end"].apply(lambda x : date(month=5, day=1, year=x))
     team_df = team_df.rename(columns={"total_points" : "y"})
     team_df["ds"] = pandas.to_datetime(team_df["ds"])
     del team_df["season_end"]
-
-    if model_training:
-        centring_mean = team_df["y"][0:-1].mean()
-        centring_std = team_df["y"][0:-1].std()
-    else:
-        centring_mean = team_df["y"].mean()
-        centring_std = team_df["y"].std()
-
-    team_df["y"] = (team_df["y"] - centring_mean) / centring_std
 
     team_df["lag1"] = team_df["y"].shift()
     if model_training:
@@ -62,7 +52,7 @@ def preprocess(team_df, model_training):
     if model_training:
         train_df = team_df[0:-1].copy()
         test_df = team_df[-1:].copy()
-        return (train_df, test_df, centring_mean, centring_std)
+        return (train_df, test_df)
 
     forecast_df = pandas.DataFrame([{"ds" : team_df.iloc[-1]["ds"],
                                      "lag1" : team_df.iloc[-1]["y"],
@@ -70,7 +60,7 @@ def preprocess(team_df, model_training):
                                      "lag3" : team_df.iloc[-1]["lag2"]}])
     forecast_df["ds"] = forecast_df["ds"].apply(lambda x : date(year=x.year + 1, month=x.month, day=x.day))
     forecast_df["ds"] = pandas.to_datetime(forecast_df["ds"])
-    return (team_df, forecast_df, centring_mean, centring_std)
+    return (team_df, forecast_df)
 
 
 if __name__ == "__main__":
